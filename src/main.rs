352a91a777a6f0d24e8cb4e7d19287f4e259cae5
Emad Ali <70924991+Mclilzee@users.noreply.github.com>
2025-05-10 07:55:15 +0200

    Create getting context response

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ mod meta_data;
 use anyhow::Result;
 use args::Args;
 use clap::Parser;
-use meta_data::{Data, MetaData};
+use meta_data::{ContextResponse, MetaData};
 use prettydiff::diff_lines;
 use reqwest::{Client, Response};
 use serde::Deserialize;
@@ -13,33 +13,37 @@ use serde::Deserialize;
 async fn main() -> Result<()> {
     let args = Args::parse();
     let json = std::fs::read_to_string(args.path).expect("Failed to read json file");
-    let meta_data: Vec<MetaData> =
+    let meta_data: Vec<(MetaData, MetaData)> =
         serde_json::from_str(&json).expect("Json is not formatted correctly");
-    meta_data.into_iter().for_each(print_content);
 
     let client = reqwest::Client::new();
-    let response = get_content(&client, "https://demoqa.com/").await?;
-    println!(
-        "status code: {:#?}\nbody: {:#?}",
-        response.status(),
-        response.text().await?
-    );
+    meta_data.into_iter().map(|(left, right)| {
+        tokio::spawn({
+            let moved_client = client.clone();
+            async move {
+                let left_context = get_context_response(&moved_client, left).await;
+                let right_context = get_context_response(&moved_client, right).await;
+
+                (left_context, right_context)
+            }
+        })
+    });
 
     Ok(())
 }
 
-async fn get_content(client: &Client, url: &str) -> Result<Response> {}
-
-fn print_content(meta_data: MetaData) {
-    let inline_change = diff_lines(&meta_data.left.url, &meta_data.right.url)
+fn print_content(meta_data: (MetaData, MetaData)) {
+    let inline_change = diff_lines(&meta_data.0.url, &meta_data.1.url)
         .set_show_lines(true)
-        .names("lefsdfsdfsdft", "right")
+        .names(&meta_data.0.name, &meta_data.1.name)
         .prettytable();
 
     println!("{inline_change:?}");
 }
 
-fn call_url(client: &Client, data: Data) {
-    let response = client.get(data.url).send().await?;
-    Ok(response)
+async fn get_context_response(client: &Client, data: MetaData) -> Result<ContextResponse> {
+    let response = client.get(&data.url).send().await?;
+    let status_code = response.status();
+    let text = response.text().await?;
+    Ok(ContextResponse::new(data.name, status_code, text))
 }
--- src/meta_data.rs
+++ src/meta_data.rs
@@ -1,16 +1,29 @@
+use anyhow::Result;
+use reqwest::{Response, StatusCode};
 use serde::Deserialize;
 
 #[derive(Deserialize)]
 pub struct MetaData {
-    pub left: Data,
-    pub right: Data,
-}
-
-#[derive(Deserialize)]
-pub struct Data {
     pub name: String,
     pub url: String,
     pub ignore: Option<Vec<String>>,
     pub format: Option<bool>,
     pub cached: Option<bool>,
+    pub headers: Option<Vec<(String, String)>>,
+}
+
+pub struct ContextResponse {
+    pub name: String,
+    pub status_code: StatusCode,
+    pub text: String,
+}
+
+impl ContextResponse {
+    pub fn new(name: String, status_code: StatusCode, text: String) -> Self {
+        Self {
+            name,
+            status_code,
+            text,
+        }
+    }
 }
